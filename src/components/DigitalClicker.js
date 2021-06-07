262aa0a7d0a9f3192b21c8fb7023b73ba33c08c2
Omer Sabah <omerrj99@gmail.com>
2021-06-07 23:11:53 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,14 @@
-// Code DigitalClicker Component Here
+import React,{useState} from 'react';
+
+export default function() {
+const [timesClicked,setTimesClicked]=useState(0)
+
+  const handleClick = () => {
+    setTimesClicked(timesClicked+1)
+  }
+
+    return (
+      <button onClick={handleClick}>{timesClicked}</button>
+    );
+  
+}
